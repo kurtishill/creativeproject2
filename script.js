0558e068080f1622c71f9308d558da724618821b
Kurtis Hill <kurtis.hill212@gmail.com>
2018-02-12 19:04:05 -0700

    fixed problem

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ $(document).ready(function() {
 		$.ajax({
 			url : myUrl,
 			dataType : "json",
-			headers : { 'Access-Control-Allow-Origin': 'http://138.197.216.228 allowed to access' },
+			crossDomain : true,
+			headers : { 'Access-Control-Allow-Origin': '*' },
 			success : function(json) {
 				console.log(json);
 				results = "";
